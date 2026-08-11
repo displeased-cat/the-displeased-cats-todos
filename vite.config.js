4e0fb33ef72d25590e9bb8ff0d47d1ce82c268cd
Luca <130316165+displeased-cat@users.noreply.github.com>
2026-08-11 09:53:13 +0000

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/the-displeased-cats-todos/',
   plugins: [react()],
   test: {
     globals: true,
